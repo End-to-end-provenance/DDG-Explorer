d09bab4800757c043495da139df1aa98c3f1e2c6
Moe <julia.moepwint@gmail.com>
2016-06-27 16:31:18 -0400

    Created Getter Methods for fileName and timeStamp in DDGServer

--- src/laser/ddg/DDGServer.java
+++ src/laser/ddg/DDGServer.java
@@ -10,18 +10,32 @@ import java.net.Socket;
 public class DDGServer extends ServerSocket{
 	
 	String s = "";
+	String fileName;
+	String timeStamp;
+	Socket clientSocket;
 	public DDGServer(int portNumber) throws IOException{
 		super(portNumber);
-		Socket clientSocket = this.accept();
+		clientSocket = this.accept();
 		BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 		if(clientSocket.isConnected()){
 			System.out.println("YAY CONNECTED");
 		}
+		fileName = in.readLine();
+		timeStamp = in.readLine();
 		while((s =in.readLine())!=null){
 			System.out.println(s);
 		}
 		this.close();
 	}
 	
+	public Socket getClientSocket(){
+		return clientSocket;
+	}
+	public String getFileName(){
+		return fileName;
+	}
 
+	public String getTimeStamp(){
+		return timeStamp;
+	}
 }
