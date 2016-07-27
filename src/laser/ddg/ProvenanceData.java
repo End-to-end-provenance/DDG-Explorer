2a2da1e34a27eeb0c1b5d5ac974e512433e0cf9b
Barbara Lerner <blerner@mtholyoke.edu>
2016-07-27 12:15:47 -0400

    Code cleanup.

--- src/laser/ddg/ProvenanceData.java
+++ src/laser/ddg/ProvenanceData.java
@@ -3,7 +3,6 @@ package laser.ddg;
 import java.io.File;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
