e21d367576ad6bb2e523881b61eac22fd1710d54
Moe <julia.moepwint@gmail.com>
2016-07-29 13:12:24 -0400

    Cleaned the overlapping code for loadFileCommand

--- src/laser/ddg/commands/LoadFileCommand.java
+++ src/laser/ddg/commands/LoadFileCommand.java
@@ -3,11 +3,13 @@ package laser.ddg.commands;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.IOException;
 import java.net.ServerSocket;
 
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
+import laser.ddg.ClientConnection;
 import laser.ddg.DDGServer;
 import laser.ddg.gui.DDGExplorer;
 import laser.ddg.persist.JenaWriter;
@@ -25,13 +27,14 @@ import laser.ddg.visualizer.PrefuseGraphBuilder;
 public class LoadFileCommand implements ActionListener {
 
 	private static final JFileChooser FILE_CHOOSER = new JFileChooser(System.getProperty("user.dir"));
+	private static JenaWriter jenaWriter = JenaWriter.getInstance();
+	private static DDGExplorer ddgExplorer = DDGExplorer.getInstance();
 
 	/**
 	 * Loads a ddg text file from user selection
 	 * @throws Exception thrown if the file cannot be loaded
 	 */
 	public static void execute() throws Exception {
-		DDGExplorer ddgExplorer = DDGExplorer.getInstance();
 		if (FILE_CHOOSER.showOpenDialog(ddgExplorer) == JFileChooser.APPROVE_OPTION) {
 			File selectedFile = FILE_CHOOSER.getSelectedFile();
 			loadFile(selectedFile);
@@ -54,45 +57,52 @@ public class LoadFileCommand implements ActionListener {
 	 * @throws Exception
 	 */
 	public static void loadFile(File selectedFile) throws Exception{
-		JenaWriter jenaWriter = JenaWriter.getInstance();
-		DDGExplorer ddgExplorer = DDGExplorer.getInstance();
 		PrefuseGraphBuilder builder = new PrefuseGraphBuilder(false, jenaWriter);
 		String selectedFileName = selectedFile.getName();
-		DDGExplorer.loadingDDG();
-		builder.processStarted(selectedFileName, null);
 		Parser parser = new Parser(selectedFile, builder);
+		startLoadingDDG(builder,selectedFileName);
 		parser.addNodesAndEdges();
-		
-		//new tab!
-		ddgExplorer.addTab(builder.getPanel().getName(), builder.getPanel());
-		DDGExplorer.doneLoadingDDG();
+		finishLoadingDDG(builder);
 	}
 
-	public static void executeIncrementalDrawing(DDGServer ddgSocket) throws Exception {
-		System.out.println("FILE NAME" + ddgSocket.getFileName());
-		JenaWriter jenaWriter = JenaWriter.getInstance();
-		DDGExplorer ddgExplorer = DDGExplorer.getInstance();
+	/**
+	 * for incremental drawing, build everything using information from the client side 
+	 * @param clientConnection
+	 * @throws Exception
+	 */
+	public static void executeIncrementalDrawing(ClientConnection clientConnection) throws Exception {
+		PrefuseGraphBuilder builder = new PrefuseGraphBuilder(true, jenaWriter,clientConnection.getClientReader());
+		String selectedFileName = clientConnection.getFileName();
+		Parser parser = new Parser(clientConnection, builder);
+		builder.setTitle(selectedFileName, clientConnection.getTimeStamp());
+		startLoadingDDG(builder,selectedFileName);
+		finishLoadingDDG(builder);
+		parser.addNodesAndEdges();		
+	}
 
-		PrefuseGraphBuilder builder = new PrefuseGraphBuilder(true, jenaWriter,ddgSocket.getClientReader());
-		String selectedFileName = ddgSocket.getFileName();
 
+	/**
+	 * preparation to start loading DDG
+	 * @param builder
+	 * @param parser
+	 * @param fileName
+	 * @throws IOException
+	 */
+	public static void startLoadingDDG(PrefuseGraphBuilder builder,String fileName) throws IOException{
 		DDGExplorer.loadingDDG();
-		builder.setTitle(selectedFileName, ddgSocket.getTimeStamp());
-		builder.processStarted(selectedFileName, null);
-		
-		Parser parser = new Parser(ddgSocket, builder);
+		builder.processStarted(fileName, null);
+	}
+	
+	/**
+	 * process to finish loading DDG
+	 * @param builder
+	 */
+	public static void finishLoadingDDG(PrefuseGraphBuilder builder){
 		//new tab!
 		ddgExplorer.addTab(builder.getPanel().getName(), builder.getPanel());
-		
-		// Hmmm.  Do we want to call this here?
 		DDGExplorer.doneLoadingDDG();
-		
-		parser.addNodesAndEdgesForIncrementalDrawing(ddgSocket);
-		
-		System.out.println("Server closing");
-		ddgSocket.close();
+	
 	}
-
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
