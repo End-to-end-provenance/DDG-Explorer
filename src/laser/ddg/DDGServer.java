eba7dde40948ba7ec97c5513fcd271e5140057ce
Moe <julia.moepwint@gmail.com>
2016-07-29 09:34:34 -0400

    Created server to accept multiple clients

--- src/laser/ddg/DDGServer.java
+++ src/laser/ddg/DDGServer.java
@@ -3,62 +3,31 @@ package laser.ddg;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * This class contains the code that starts the server and accepts multiple clients
+ * @author Moe Pwint Phyu 
+ *
+ */
 public class DDGServer extends ServerSocket{
 	
-	String s = "";
-	String fileName;
-	String timeStamp;
-	Socket clientSocket;
-	private BufferedReader in;
-	
-	
+	private Socket clientSocket;
+	private ClientConnection clientConnection;
+
 	public DDGServer(int portNumber) throws IOException{
 		super(portNumber);
-		clientSocket = this.accept();
-		if(clientSocket.isConnected()){
-			System.out.println("YAY CONNECTED");
+		//accept multiple clients 
+		while(true){
+			clientSocket = this.accept();
+			clientConnection = new ClientConnection(clientSocket);
+			new Thread(clientConnection).start();
 		}
-		in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		System.out.println("Server got input stream");
-//		try {
-			System.out.println("Calling readLine");
-			fileName = in.readLine();
-//			System.out.println("readLine returned");
-//			while (fileName == null) {
-//				System.out.println("Server waiting for filename");
-//				Thread.sleep(1000);
-//				fileName = in.readLine();
-//			}
-			System.out.println("Got filename");
-			timeStamp = in.readLine();
-			System.out.println("Got timestamp");
-//		while((s =in.readLine())!=null){
-//			System.out.println(s);
-//		}
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
 	}
 	
-	public Socket getClientSocket(){
-		return clientSocket;
+	public ClientConnection getClientConnection(){
+		return this.clientConnection;
 	}
-	public String getFileName(){
-		return fileName;
-	}
-
-	public String getTimeStamp(){
-		return timeStamp;
-	}
-	
-	public BufferedReader getClientReader() {
-		return in;
-	}
-	
-
 }
+
