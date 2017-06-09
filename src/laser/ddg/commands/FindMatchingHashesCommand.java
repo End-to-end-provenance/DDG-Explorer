eccbe5fbfaad3cd9109eb3c436f805d6b627c358
Connor G-T <CGT@Connors-MacBook-Pro-4.local>
2017-06-09 15:38:44 -0400

    read hashtable hashes

--- src/laser/ddg/commands/FindMatchingHashesCommand.java
+++ src/laser/ddg/commands/FindMatchingHashesCommand.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import laser.ddg.gui.DDGExplorer;
@@ -17,25 +18,40 @@ public class FindMatchingHashesCommand implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent args0) {
 		// Based off of FindTimeCommand. This only finds the nodes/snapshot csv files, however.
+		/*
 		DDGPanel panel = DDGExplorer.getCurrentDDGPanel();
 		ArrayList<SearchElement> nodeList = 
 				(ArrayList<SearchElement>) panel.getSearchIndex().getFileList().clone();
 		for (int i = 0; i < nodeList.size(); i++) {
 			String name = nodeList.get(i).getName();
 		}
-		
-		
+		 */
+		try {
+			readHashtable();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void readHashtable() throws IOException {
+		// https://www.javatpoint.com/java-bufferedreader-class
+		// https://www.mkyong.com/java/how-to-read-and-parse-csv-file-in-java/
 		String home = System.getProperty("user.home");
 		File hashtable = new File(home + "/.ddg/hashtable.csv");
 		System.out.println(hashtable.getAbsolutePath());
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(hashtable));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+		String line = "";
+		String[] entries = null;
+
+		FileReader fr = new FileReader(hashtable);
+		BufferedReader br = new BufferedReader(fr);
+		br.readLine(); // To 
+		while((line = br.readLine()) != null) {
+			entries = line.split(",");
+			System.out.println(entries[4]);
 		}
-		
-		
+		br.close();
 	}
-	
+
 
 }
