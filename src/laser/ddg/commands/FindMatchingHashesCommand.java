9f11a6d80426b296090cbda48f5cfbb163a1088c
Connor G-T <CGT@Connors-MacBook-Pro-4.local>
2017-06-09 10:40:44 -0400

    Finds file nodes

--- src/laser/ddg/commands/FindMatchingHashesCommand.java
+++ src/laser/ddg/commands/FindMatchingHashesCommand.java
@@ -2,12 +2,21 @@ package laser.ddg.commands;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import laser.ddg.gui.DDGExplorer;
+import laser.ddg.gui.DDGPanel;
+import laser.ddg.search.SearchElement;
 
 public class FindMatchingHashesCommand implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent args0) {
-		System.exit(0);
+		// Based off of FindTimeCommand
+		DDGPanel panel = DDGExplorer.getCurrentDDGPanel();
+		ArrayList<SearchElement> nodeList = 
+				(ArrayList<SearchElement>) panel.getSearchIndex().getFileList().clone();
+		
 	}
 	
 
